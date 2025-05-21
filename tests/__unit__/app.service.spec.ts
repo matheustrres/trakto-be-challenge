5b8ffe91a517776d8d81f6ef605a27e5734cf2de
Matheus Torres <matheustrres.rj@gmail.com>
2025-05-21 14:46:15 -0300

    test: ensure SUT throws an error if no file is provided

---
+++ tests/__unit__/app.service.spec.ts
@@ -0,0 +1,50 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppService } from '@/app.service';
+
+import { ImageProducerService } from '@/shared/libs/rmq/services/image-producer.service';
+import { ImageTask } from '@/shared/modules/database/schemas/image-task.schema';
+
+describe(AppService.name, () => {
+	let appService: AppService;
+	let imageProducerService: ImageProducerService;
+
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			providers: [
+				AppService,
+				{
+					provide: getModelToken(ImageTask.name),
+					useValue: {},
+				},
+				{
+					provide: ImageProducerService,
+					useValue: {
+						enqueue: jest.fn(),
+					},
+				},
+			],
+		}).compile();
+
+		appService = app.get<AppService>(AppService);
+		imageProducerService = app.get<ImageProducerService>(ImageProducerService);
+	});
+
+	it('should be defined', () => {
+		expect(appService).toBeDefined();
+	});
+
+	describe('.enqueueImage', () => {
+		it('should throw an error if no file is provided', async () => {
+			jest.spyOn(imageProducerService, 'enqueue');
+
+			const file = null!;
+
+			await expect(appService.enqueueImage(file)).rejects.toThrow(
+				'File is required',
+			);
+			expect(imageProducerService.enqueue).not.toHaveBeenCalled();
+		});
+	});
+});
